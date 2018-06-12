2d25e93313fc1e2849cbb242b36d35891270d0b9
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-12 21:04:37 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -2,7 +2,7 @@ class RPSGame
   @@valid_plays = ["rock", "paper", "scissors"]
   
   def initialize(choice)
-    if @choice == "rock" || @choice == "paper" || @choice == "scissors"
+    if choice == "rock" || choice == "paper" || choice == "scissors"
       @choice = choice
     else 
       raise PlayTypeError
